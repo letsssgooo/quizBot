737e7393a2cf6ce4abf3b60a47ac225720f264b1
Ivan Barbashin <ivanbarbash06@gmail.com>
2026-01-19 20:45:37 +0300

    Init users table in migrations

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    telegram_username VARCHAR(32) NOT NULL,
+    full_name VARCHAR(150),
+    role VARCHAR(20) NOT NULL,
+    student_group VARCHAR,
+    created_at TIMESTAMP NOT NULL
+);
